1e5db57f94b3d7020592478f548e82162b5d21e8
dedeomkar <dedeomkar@gmail.com>
2025-03-17 21:23:48 +0530

    first commit

---
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -0,0 +1,65 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class LoginSteps {
+
+    private WebDriver driver;
+
+    @Given("I am on the login page")
+    public void iAmOnTheLoginPage() {
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver"); // Replace with your chromedriver path
+        this.driver = new ChromeDriver();
+        driver.get("https://www.boomplay.com/");
+    }
+
+    @When("I enter credentials {string} and {string}")
+    public void iEnterCredentials(String username, String password) throws InterruptedException {
+        driver.findElement(By.xpath("/html/body/article[1]/article/div/div/strong/button[1]")).click();
+        driver.findElement(By.xpath("/html/body/article[2]/form[1]/div[1]/label[2]")).click();
+        driver.findElement(By.xpath("/html/body/article[2]/form[1]/div[3]/label/input")).sendKeys(username);
+        driver.findElement(By.xpath("/html/body/article[2]/form[1]/div[4]/label/input")).sendKeys(password);
+    }
+
+    @When("I click the login button")
+    public void iClickTheLoginButton() {
+        driver.findElement(By.xpath("/html/body/article[2]/form[1]/div[6]/input")).click();
+    }
+
+    @Then("I should be redirected to the dashboard")
+    public void iShouldBeRedirectedToTheDashboard() {
+        WebElement loginForm = driver.findElement(By.xpath("/html/body/article[2]"));
+        assertFalse(loginForm.getAttribute("class").contains("show"));
+    }
+
+    @Then("I should see a welcome message {string}")
+    public void iShouldSeeAWelcomeMessage(String message) {
+        assertEquals(driver.findElement(By.xpath("/html/body/article[1]/article/a/div[2]/strong")).getText(), message);
+        driver.quit();
+    }
+
+    @Then("I should see an error message {string}")
+    public void iShouldSeeAnErrorMessage(String message) {
+        WebElement loginForm = driver.findElement(By.xpath("/html/body/article[2]"));
+        assertTrue(loginForm.getAttribute("class").contains("show"));
+        driver.quit();
+    }
+
+    @And("Wait for {int}")
+    public void waitFor(int arg0) throws InterruptedException {
+        Thread.sleep(3000);
+    }
+}
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,30 @@
+Feature: User Login
+
+  As a user
+  I want to be able to log in to the website
+  So that I can access my account
+
+#Given: Sets up the initial situation. (e.g., "Given I am logged in.")
+#When: Describes an action. (e.g., "When I click the 'Submit' button.")
+#Then: Describes the expected outcome. (e.g., "Then I should see a success message.")
+#And/But: Adds extra steps or exceptions.
+
+
+  Scenario: Successful Login
+    Given I am on the login page
+    And Wait for 5000
+    When I enter credentials "mblaze804@gmail.com" and "qwerqwer"
+    And Wait for 2000
+    And I click the login button
+    And Wait for 5000
+    Then I should be redirected to the dashboard
+    And I should see a welcome message "mblaze804"
+
+  Scenario: Failed Login
+    Given I am on the login page
+    And Wait for 5000
+    When I enter credentials "wronguser" and "wrongpassword"
+    And Wait for 2000
+    And I click the login button
+    And Wait for 5000
+    Then I should see an error message "Invalid credentials"
